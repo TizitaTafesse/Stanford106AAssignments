167c2ae346b3f1627d28927c0788759471468f6f
Tizita Tafesse <tizitatafesse@gmail.com>
2021-02-27 21:54:20 -0500

    week 3

--- src/week3/AddCommasToNumberStrings.java
+++ src/week3/AddCommasToNumberStrings.java
@@ -1,11 +1,22 @@
 package week3;
+import DecimalFormart;
+
 
 import acm.program.ConsoleProgram;
 
 public class AddCommasToNumberStrings extends ConsoleProgram {
 	
-	public void run() {
+ // This program will separate the digits entered by user into groups of three.	
+		public String AddCommasToNumberStrings(String index) {
+		    String result = "";
+		    for (int i=1; i <= index.length(); ++i) {
+		        char ch = index.charAt(index.length() - i);
+		        if (i % 3 == 1 && i > 1) {
+		            result = "," + result;
+		        }
+		        result = ch + result;
+		    }
 
+		    return result;
+		}
 	}
-	
-}
--- src/week3/DeleteCharactersFromString.java
+++ src/week3/DeleteCharactersFromString.java
@@ -5,6 +5,16 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
+		// this code will remove all characters of the String
+		String removeAllOccurrences (String str; char ch){
+			String Outcome = "";
+			for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ch) {
+			Outcome += str.charAt(i);
+			}
+			}
+			println ( Outcome );
+			}
 
 	}
 	
