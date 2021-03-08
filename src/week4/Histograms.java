fbec4dca064b3614a6767c568a5e8bc76e923f0b
Tizita Tafesse <tizitatafesse@gmail.com>
2021-03-08 15:50:49 -0500

    week 4

--- src/week4/Histograms.java
+++ src/week4/Histograms.java
@@ -1,11 +1,38 @@
 package week4;
 
+import java.io.BufferedReader;
+
 import acm.program.ConsoleProgram;
+
 
 public class Histograms extends ConsoleProgram {
 	
-	public void run() {
-
-	}
+ /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+public void run() {
+ initHistogram();
+ readScoresIntoHistogram3();
+ printHistogram2();
+ }
+private void printHistogram2() {
+	// TODO Auto-generated method stub
 	
 }
+private void readScoresIntoHistogram3() {
+	// TODO Auto-generated method stub
+	
+}
+
+ private void initHistogram() {
+ int[] histogramArray = new int[11];
+ for (int i = 0; i <= 10; i++) {
+ histogramArray[i] = 0;
+ }
+ }
+ 
+ private int[] histogramArray;
+
+ private static final String DATA_FILE = "MidtermScores.txt";
+} 
--- src/week4/ImageProcessing.java
+++ src/week4/ImageProcessing.java
@@ -1,7 +1,37 @@
 package week4;
 
+import acm.graphics.GImage;
 import acm.program.GraphicsProgram;
 
 public class ImageProcessing extends GraphicsProgram {
+
+        public static final int APPLICATION_HEIGHT = 350; //setting window size
+        public static final int APPLICATION_WIDTH = 500;//setting window size
+
+        public void run() {
+                int x = (int)(getWidth() - APPLICATION_WIDTH / 2);
+                int y = (int)(getHeight() - APPLICATION_HEIGHT /2);
+
+                GImage image = new GImage("housewife.png"); //saved on my desktop
+                add(image, 0, 0); 
+                add(flipHorizontally(image), 250, 0); //Flippinig the picture horizontally
+        }
+
+
+        private GImage flipHorizontally(GImage image) {
+                int[][]array = image.getPixelArray();
+                int height = array.length;
+                int width = array[0].length;
+
+                for(int row = 0; row < height; row++) {
+                        for(int i = 0; i < width /2; i++) {
+                                int flipped = width - i - 1;
+                                int temp = array[row][i];
+                                array[row][i] = array[row][flipped];
+                                array[row][flipped] = temp;
+                        }
+                }               
+                return new GImage(array);
+        }       
 
 }
